8a2e96b62a51b600bb306bb92226e4d9b08a67dc
sanjaysajank <sanjaysanju527265@gmail.com>
2026-06-25 21:22:29 +0530

    Fix Render deployment

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, request
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer
-
+import nltk
+nltk.download('punkt')
+nltk.download('punkt_tab')
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,5 +23,8 @@ def home():
 
     return render_template('index.html', summary=summary)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
